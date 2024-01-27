b4b542e78f11b7e279a63843538e9614bfae844a
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-01-27 15:26:59 -0500

    msking part one

--- src/GamePartOne.js
+++ src/GamePartOne.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { useState } from "react";
 
 const GamePartOne = () => {
+  const [onstart, SetStart] = useState(true);
+
   return (
     <div>
       <h1> 
         Welcome to my game! you start with nothing! 
         and Are Nothing play the Game to become Something
       </h1>
+      <div>
+        <button>Inventory</button>
+        <div className="Inv-Tooltip"></div>
+      </div>
       <div className="GameBoard">
-        <div className="Map-places">Start</div>
+        <div className="Map-places Start">Start</div>
+        <div className="Map-places P1A1">Chest</div>
+        <div className="Map-places P1A2">Fight</div>
+        <div className="Map-places P1A3">Encounter</div>
+        <div className="Map-places P1A4">Town1</div>
       </div>
     </div>
   )
