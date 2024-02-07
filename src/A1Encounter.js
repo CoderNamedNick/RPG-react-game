f0d489f6ac5392cb185194aa6366009d6a700eb7
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-02-07 18:42:49 -0500

    added eyes and smile

--- src/A1Encounter.js
+++ src/A1Encounter.js
@@ -1,4 +1,7 @@
 import React from "react";
+import eye from './eye-ball.png'
+import mouth from './mouth-smile-clip-art-nicubunu_Mouth_with_teeth.png'
+import coin from './naira.png'
 
 const A1Encounter = ({onReturn}) => {
 
@@ -7,8 +10,12 @@ const A1Encounter = ({onReturn}) => {
   };
 
   return (
-    <div>
+    <div className="Main-A1-Encounter-Div">
       hello
+      <img className="eye" src={eye} alt="eye"></img>
+      <img className="eye2" src={eye} alt="eye"></img>
+      <img className="mouth" src={mouth} alt="mouth"></img>
+      <img className="coin" src={coin} alt="coin"></img>
       <button onClick={handleReturn}>return</button>
     </div>
   )
