2ad7beaaa1f4de90435d4cfd2bf9f21aefb66c5e
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-02-08 18:17:50 -0500

    made heads or tails game

--- src/A1Encounter.js
+++ src/A1Encounter.js
@@ -2,23 +2,64 @@ import { useState, useEffect } from "react";
 import mouth from './mouth-smile-clip-art-nicubunu_Mouth_with_teeth.png'
 
 const A1Encounter = ({ playerName, characterStats, updateCharacterStats, onReturn }) => {
-  const [PlayingGame, setPlayingGame] = useState(false)
+  const [PlayingGame, setPlayingGame] = useState(false);
+  const [Heads, setHeads] = useState('');
+  const [Tails, setTails] = useState('');
 
   const handleReturn = () => {
     onReturn(); //return function
   };
   const handleyesclick = () => {
+    coinflip();
     setPlayingGame(true)
   }
 
+  const coinflip = () => {
+    const randomNum = Math.ceil(Math.random() * 2);
+    if (randomNum === 1) {
+      setHeads('yes');
+      setTails('no');
+    } else {
+      setHeads('no');
+      setTails('yes');
+    }
+  };
 
   if (PlayingGame) {
     // Add your game logic here
     // You can return different content or components based on the game state
+    
+    const handlHeadsclick = () => {
+      if (Heads === 'yes') {
+        alert('You WIn')
+      }
+      if (Heads === 'no') {
+        alert('You Lose')
+      }
+      coinflip();
+    }
+    const handlTailsclick = () => {
+      if (Tails === 'yes') {
+        alert('You WIn')
+      }
+      if (Tails === 'no') {
+        alert('You Lose')
+      }
+      coinflip();
+    }
+
+
     return (
       <div className="Main-A1-Encounter-Div">
+        <p className="Moms">I WoNt ChEaT</p>
         <img className="mouth" src={mouth} alt="mouth"></img>
         {/* Display game content or components */}
+        <h2 onClick={handlHeadsclick} className="h2Head">
+        Heads
+      </h2>
+      <h2 onClick={handlTailsclick} className="h2Tail">
+        Tails
+      </h2>
       </div>
     );
   }
