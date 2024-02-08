9a4cb6ba0b839469ccfb9989736fa7f8b17cd053
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-02-08 17:54:20 -0500

    made start game

--- src/A1Encounter.js
+++ src/A1Encounter.js
@@ -1,35 +1,53 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import mouth from './mouth-smile-clip-art-nicubunu_Mouth_with_teeth.png'
 
 const A1Encounter = ({ playerName, characterStats, updateCharacterStats, onReturn }) => {
+  const [PlayingGame, setPlayingGame] = useState(false)
 
   const handleReturn = () => {
     onReturn(); //return function
   };
+  const handleyesclick = () => {
+    setPlayingGame(true)
+  }
+
+
+  if (PlayingGame) {
+    // Add your game logic here
+    // You can return different content or components based on the game state
+    return (
+      <div className="Main-A1-Encounter-Div">
+        <img className="mouth" src={mouth} alt="mouth"></img>
+        {/* Display game content or components */}
+      </div>
+    );
+  }
 
   return (
     <div className="Main-A1-Encounter-Div">
       <img className="mouth" src={mouth} alt="mouth"></img>
       <p className="Moms">pLaY A gAme WiTh Me</p>
-      {//<button onClick={handleReturn}>return</button>}
-      }   
-      <p className="Wincon" >
+      <p className="Wincon">
         If yOu WiN I hEaL yOu!
         <br></br>
         <br></br>
         <span>Current HP: {characterStats.Hp} + 10</span>
       </p>
-      <p className="Losecon" >
+      <p className="Losecon">
         If yOu LoSe I HuRt yOu!
         <br></br>
         <br></br>
-        <span>Current HP: {characterStats.Hp} - 10</span> 
+        <span>Current HP: {characterStats.Hp} - 10</span>
       </p>
       <h1 className="Moms PLay">PLAY?</h1>
-      <h2 className="h2yes">YES</h2>
-      <h2 className="h2no">NO</h2>
+      <h2 onClick={handleyesclick} className="h2yes">
+        YES
+      </h2>
+      <h2 onClick={handleReturn} className="h2no">
+        NO
+      </h2>
     </div>
-  )
+  );
 }
 
 export default A1Encounter;
