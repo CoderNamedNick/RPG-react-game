78f77fc5a3af9e335683be5f09c504f49b469f88
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-02-09 13:46:22 -0500

    made second gamework

--- src/A1Encounter.js
+++ src/A1Encounter.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import mouth from './mouth-smile-clip-art-nicubunu_Mouth_with_teeth.png'
+import A1HealthManaBars from "./A1HealthManaBars";
 
 const A1Encounter = ({ playerName, characterStats, updateCharacterStats, onReturn }) => {
   const [PlayingGame, setPlayingGame] = useState(false);
   const [Heads, setHeads] = useState('');
   const [Tails, setTails] = useState('');
   const [SecondGame, setSecondGame] = useState(false);
+  const [SecondPlayingGame, setSecondPlayingGame] = useState(false);
+  const [ThirdGame, setThirdGame] = useState(false);
 
   const handleReturn = () => {
     onReturn(); //return function
@@ -13,6 +16,10 @@ const A1Encounter = ({ playerName, characterStats, updateCharacterStats, onRetur
   const handleyesclick = () => {
     coinflip();
     setPlayingGame(true)
+  }
+  const handleyesclick2 = () => {
+    coinflip();
+    setSecondPlayingGame(true)
   }
 
   const coinflip = () => {
@@ -26,12 +33,113 @@ const A1Encounter = ({ playerName, characterStats, updateCharacterStats, onRetur
     }
   };
 
+  if (SecondPlayingGame) {
+    // Add your game logic here
+    // You can return different content or components based on the game state
+    
+    const handlHeadsclick = () => {
+      if (Heads === 'yes') {
+        alert('You WIn')
+        const updatedStats = {
+          ...characterStats,
+          Hp: characterStats.Hp + 10,
+          Atk: characterStats.Atk + 5,
+        };
+        updateCharacterStats(updatedStats);
+        setThirdGame(true)
+      }
+      if (Heads === 'no') {
+        alert('You Lose')
+        const updatedStats = {
+          ...characterStats,
+          Hp: characterStats.Hp - 20,
+        };
+        updateCharacterStats(updatedStats);
+        setThirdGame(true)
+      }
+      coinflip();
+    }
+    const handlTailsclick = () => {
+      if (Tails === 'yes') {
+        alert('You WIn')
+        const updatedStats = {
+          ...characterStats,
+          Hp: characterStats.Hp + 10,
+          Atk: characterStats.Atk + 5,
+        };
+        updateCharacterStats(updatedStats);
+        setThirdGame(true)
+      }
+      if (Tails === 'no') {
+        alert('You Lose')
+        const updatedStats = {
+          ...characterStats,
+          Hp: characterStats.Hp - 20,
+        };
+        updateCharacterStats(updatedStats);
+        setThirdGame(true)
+      }
+      coinflip();
+    }
+
+
+    return (
+      <div className="Main-A1-Encounter-Div">
+        <p className="Moms">I WoNt ChEaT</p>
+        <img className="mouth" src={mouth} alt="mouth"></img>
+        {/* Display game content or components */}
+        <div>
+          <A1HealthManaBars 
+            playerName={playerName}
+            characterStats={characterStats}
+          />
+        </div>
+        <h2 onClick={handlHeadsclick} className="h2Head">
+        Heads
+      </h2>
+      <h2 onClick={handlTailsclick} className="h2Tail">
+        Tails
+      </h2>
+      </div>
+    );
+  }
+
 
   if (SecondGame) {
     return (
+    <div className="Main-A1-Encounter-Div">
+      <img className="mouth2" src={mouth} alt="mouth"></img>
+      <p className="Moms">PLAY AGAIN!</p>
       <div>
-        wanna play again
+        <A1HealthManaBars 
+          playerName={playerName}
+          characterStats={characterStats}
+        />
       </div>
+      <p className="Wincon">
+        If yOu WiN I ReWarD yOu!
+        <br></br>
+        <br></br>
+        <span>Current HP: {characterStats.Hp} + 10</span>
+        <br></br>
+        <span>and</span>
+        <br></br>
+        <span>Current ATK: {characterStats.Atk} + 5</span>
+      </p>
+      <p className="Losecon">
+        If yOu LoSe I HuRt yOu!
+        <br></br>
+        <br></br>
+        <span>Current HP: {characterStats.Hp} - 20</span>
+      </p>
+      <h1 className="Moms PLay">PLAY?</h1>
+      <h2 onClick={handleyesclick2} className="h2yes">
+        YES
+      </h2>
+      <h2 onClick={handleReturn} className="h2no">
+        NO
+      </h2>
+    </div>
     )
   }
 
@@ -90,6 +198,12 @@ const A1Encounter = ({ playerName, characterStats, updateCharacterStats, onRetur
         <p className="Moms">I WoNt ChEaT</p>
         <img className="mouth" src={mouth} alt="mouth"></img>
         {/* Display game content or components */}
+        <div>
+          <A1HealthManaBars 
+            playerName={playerName}
+            characterStats={characterStats}
+          />
+        </div>
         <h2 onClick={handlHeadsclick} className="h2Head">
         Heads
       </h2>
@@ -104,6 +218,12 @@ const A1Encounter = ({ playerName, characterStats, updateCharacterStats, onRetur
     <div className="Main-A1-Encounter-Div">
       <img className="mouth" src={mouth} alt="mouth"></img>
       <p className="Moms">pLaY A gAme WiTh Me</p>
+      <div>
+        <A1HealthManaBars 
+          playerName={playerName}
+          characterStats={characterStats}
+        />
+      </div>
       <p className="Wincon">
         If yOu WiN I hEaL yOu!
         <br></br>
