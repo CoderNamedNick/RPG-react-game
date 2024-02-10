6d38091c5d6ad09a4f2b381ad1feb73720cf9858
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-02-09 21:36:14 -0500

    made town appear

---
+++ src/A1Town.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+const A1Town = () => {
+
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default A1Town
--- src/GamePartOne.js
+++ src/GamePartOne.js
@@ -4,6 +4,7 @@ import A1Fight from "./A1Fight";
 import CharacterData from "./CharacterData";
 import arrow from './up-arrow.png';
 import A1Encounter from "./A1Encounter";
+import A1Town from "./A1Town";
 
 const GamePartOne = ({ playerName, characterStats, updateCharacterStats }) => {
   const [showChest, setShowChest] = useState(false);
@@ -13,6 +14,8 @@ const GamePartOne = ({ playerName, characterStats, updateCharacterStats }) => {
   const [fightDone, setFightDone] = useState(false);
   const [showEncounter, setShowEncounter] = useState(false);
   const [EncounterDone, setEncounterDone] = useState(false);
+  const [showTown, setShowTown] = useState(false);
+  const [TownDone, setTownDone] = useState(false);
 
   const handleChestClick = () => {
     setShowChest(true);
@@ -23,6 +26,7 @@ const GamePartOne = ({ playerName, characterStats, updateCharacterStats }) => {
     setShowChest(false);
     setShowFight(false);
     setShowEncounter(false);
+    setShowTown(false);
   };
 
   const handleInventoryClick = () => {
@@ -38,68 +42,83 @@ const GamePartOne = ({ playerName, characterStats, updateCharacterStats }) => {
     setShowEncounter(true);
     setEncounterDone(true);
   }
+  const handleTownClick = () => {
+    setShowTown(true);
+    setTownDone(true);
+  }
 
   return (
     <div>
-      {!showEncounter && (
+      {!showTown && (
         <div>
-          {!showFight && (
+          {!showEncounter && (
             <div>
-              {showChest ? (
-                <A1Chest
-                  playerName={playerName}
-                  onReturn={handleReturnToGame}
-                  updateCharacterStats={updateCharacterStats}
-                  characterStats={characterStats}
-                />
-              ) : (
+              {!showFight && (
                 <div>
-                  <h1>
-                    Welcome to my game! You start From the bottom and work up!
-                    I hope you enjoy my game! its a work in progress!
-                  </h1>
-                  <div className="Inv-div">
-                    <button className="Inv-btn" onClick={handleInventoryClick}>
-                      Player Stats
-                    </button>
-                  </div>
-                  <img className="Arrow" src={arrow} alt="Start From bottom go up" />
-                  <img className="Arrow2" src={arrow} alt="Start From bottom go up" />
-                  {showCharacterData && (
-                    <div className="CharacterData">
-                      <CharacterData
-                        playerName={playerName}
-                        updateCharacterStats={updateCharacterStats}
-                        characterStats={characterStats}
-                      />
+                  {showChest ? (
+                    <A1Chest
+                      playerName={playerName}
+                      onReturn={handleReturnToGame}
+                      updateCharacterStats={updateCharacterStats}
+                      characterStats={characterStats}
+                    />
+                  ) : (
+                    <div>
+                      <h1>
+                        Welcome to my game! You start From the bottom and work up!
+                        I hope you enjoy my game! its a work in progress!
+                      </h1>
+                      <div className="Inv-div">
+                        <button className="Inv-btn" onClick={handleInventoryClick}>
+                          Player Stats
+                        </button>
+                      </div>
+                      <img className="Arrow" src={arrow} alt="Start From bottom go up" />
+                      <img className="Arrow2" src={arrow} alt="Start From bottom go up" />
+                      {showCharacterData && (
+                        <div className="CharacterData">
+                          <CharacterData
+                            playerName={playerName}
+                            updateCharacterStats={updateCharacterStats}
+                            characterStats={characterStats}
+                          />
+                        </div>
+                      )}
+                      <div className="GameBoard">
+                        {!chestDone && <div className="Map-places Start">Start</div>}
+                        {!chestDone && (
+                          <div
+                            onClick={chestDone ? null : handleChestClick}
+                            className={`Map-places P1A1 ${chestDone ? "disabled" : ""}`}
+                          >
+                            Chest
+                          </div>
+                        )}
+                        {!fightDone && (
+                        <div
+                          onClick={chestDone && !fightDone ? handleFightClick : null}
+                          className={`Map-places P1A2 ${fightDone || !chestDone ? "disabled" : ""}`}
+                        >
+                          Fight
+                        </div>
+                        )}
+                        {!EncounterDone && (
+                          <div
+                          onClick={!EncounterDone && chestDone && fightDone ? handleEncounterClick : null}
+                          className={`Map-places P1A3 ${EncounterDone || !fightDone || !chestDone ? "disabled" : ""}`}
+                        >
+                          Encounter
+                        </div>
+                        )}
+                        <div 
+                          onClick={!TownDone && EncounterDone && chestDone && fightDone ? handleTownClick : null}
+                          className={`Map-places P1A4 ${TownDone || !EncounterDone || !fightDone || !chestDone ? "disabled" : ""}`}
+                        >
+                          Town1
+                        </div>
+                      </div>
                     </div>
                   )}
-                  <div className="GameBoard">
-                    {!chestDone && <div className="Map-places Start">Start</div>}
-                    {!chestDone && (
-                      <div
-                        onClick={chestDone ? null : handleChestClick}
-                        className={`Map-places P1A1 ${chestDone ? "disabled" : ""}`}
-                      >
-                        Chest
-                      </div>
-                    )}
-                    {!fightDone && (
-                    <div
-                      onClick={chestDone && !fightDone ? handleFightClick : null}
-                      className={`Map-places P1A2 ${fightDone || !chestDone ? "disabled" : ""}`}
-                    >
-                      Fight
-                    </div>
-                    )}
-                    <div
-                      onClick={!EncounterDone && chestDone && fightDone ? handleEncounterClick : null}
-                      className={`Map-places P1A3 ${EncounterDone || !fightDone || !chestDone ? "disabled" : ""}`}
-                    >
-                      Encounter
-                    </div>
-                    <div className="Map-places P1A4">Town1</div>
-                  </div>
                 </div>
               )}
             </div>
@@ -117,6 +136,14 @@ const GamePartOne = ({ playerName, characterStats, updateCharacterStats }) => {
       )}
       {showEncounter && (
         <A1Encounter
+          onReturn={handleReturnToGame}
+          playerName={playerName}
+          updateCharacterStats={updateCharacterStats}
+          characterStats={characterStats}
+        />
+      )}
+      {showTown && (
+        <A1Town
           onReturn={handleReturnToGame}
           playerName={playerName}
           updateCharacterStats={updateCharacterStats}
