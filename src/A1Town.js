525db20c38497a9c34832cf491b87c58d4f3ce66
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-02-10 22:25:48 -0500

    made shops look better

--- src/A1Town.js
+++ src/A1Town.js
@@ -6,6 +6,26 @@ const A1Town = () => {
   return (
     <div className="Main-Town-Div">
       <div className="ROAD"></div>
+      <div className="SHOPS  Shop1">
+        <div className="Door"></div>
+        <div className="Roof"></div>
+        Shop1
+      </div>
+      <div className="SHOPS  Shop2">
+        <div className="Door"></div>
+        <div className="Roof"></div>
+        Shop2
+      </div>
+      <div className="SHOPS  Shop3">
+        <div className="Door"></div>
+        <div className="Roof"></div>
+        Shop3
+      </div>
+      <div className="SHOPS  Shop4">
+        <div className="Door"></div>
+        <div className="Roof"></div>
+        Shop4
+      </div>
     </div>
   )
 }
