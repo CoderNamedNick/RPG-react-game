2402885fd9759f09b0a08becd855ad7b7bb5a5ef
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-02-17 18:41:38 -0500

    makking part two

--- src/GamePartTwo.js
+++ src/GamePartTwo.js
@@ -11,6 +11,8 @@ const GamePartTwoComponent = ({ playerName, characterStats, updateCharacterStats
   const [showCharacterData, setShowCharacterData] = useState(false);
   const [showFight, setShowFight] = useState(false);
   const [fightDone, setFightDone] = useState(false);
+  const [showFight2, setShowFight2] = useState(false);
+  const [fightDone2, setFight2Done] = useState(false);
   const [showEncounter, setShowEncounter] = useState(false);
   const [EncounterDone, setEncounterDone] = useState(false);
   const [showTown, setShowTown] = useState(false);
@@ -48,6 +50,10 @@ const GamePartTwoComponent = ({ playerName, characterStats, updateCharacterStats
     setShowFight(true);
     setFightDone(true);
   };
+  const handleFight2Click = () => {
+    setShowFight2(true);
+    setFight2Done(true);
+  };
 
   const handleEncounterClick = () => {
     setShowEncounter(true);
@@ -65,70 +71,82 @@ const GamePartTwoComponent = ({ playerName, characterStats, updateCharacterStats
         <div>
           {!showEncounter && (
             <div>
-              {!showFight && (
+              {!showFight2 && (
                 <div>
-                  <h1>
-                    PartTwo
-                  </h1>
-                  <div className="Inv-div">
-                    <button className="Inv-btn" onClick={handleInventoryClick}>
-                      Player Stats
-                    </button>
-                  </div>
-                  <div className="Map-bars">
-                    <A1HealthManaBars 
-                      playerName={playerName}
-                      characterStats={characterStats}
-                    />
-                  </div>
-                  <div className="current-position">
-                    <img className={
-                      (!fightDone && !EncounterDone) ? "Arrow2" :
-                      (fightDone && !EncounterDone) ? "Arrow3" :
-                      (EncounterDone ? "Arrow4" : "Arrow")
-                    } src={arrow} alt="Start From bottom go up" />
-                    <p className={
-                      (!fightDone && !EncounterDone) ? "here2" :
-                      (fightDone && !EncounterDone) ? "here3" :
-                      (EncounterDone ? "here4" : "Here-p")
-                    }>you are here</p>
-                  </div>
-                  {showCharacterData && (
-                    <div className="CharacterData">
-                      <CharacterData
-                        playerName={playerName}
-                        updateCharacterStats={updateCharacterStats}
-                        characterStats={characterStats}
-                      />
+                  {!showFight && (
+                    <div>
+                      <h1>
+                        PartTwo
+                      </h1>
+                      <div className="Inv-div">
+                        <button className="Inv-btn" onClick={handleInventoryClick}>
+                          Player Stats
+                        </button>
+                      </div>
+                      <div className="Map-bars">
+                        <A1HealthManaBars 
+                          playerName={playerName}
+                          characterStats={characterStats}
+                        />
+                      </div>
+                      <div className="current-position">
+                        <img className={
+                          (!fightDone && !EncounterDone) ? "Arrow2" :
+                          (fightDone && !EncounterDone) ? "Arrow3" :
+                          (EncounterDone ? "Arrow4" : "Arrow")
+                        } src={arrow} alt="Start From bottom go up" />
+                        <p className={
+                          (!fightDone && !EncounterDone) ? "here2" :
+                          (fightDone && !EncounterDone) ? "here3" :
+                          (EncounterDone ? "here4" : "Here-p")
+                        }>you are here</p>
+                      </div>
+                      {showCharacterData && (
+                        <div className="CharacterData">
+                          <CharacterData
+                            playerName={playerName}
+                            updateCharacterStats={updateCharacterStats}
+                            characterStats={characterStats}
+                          />
+                        </div>
+                      )}
+                      <div className="GameBoard">
+                        <div className="Swamp-water"><p className="swamp-p">SWAMP</p></div>
+                        <div className="Swamp-water2"><p className="swamp-p2">SWAMP</p></div>
+                        <div className="map-Path"></div>
+                        {!fightDone && (
+                        <div
+                          onClick={!fightDone ? handleFightClick : null}
+                          className={`Map-places P1A1 ${fightDone ? "disabled" : ""}`}
+                        >
+                          Fight
+                        </div>
+                        )}
+                        {!fightDone2 && (
+                        <div
+                          onClick={!fightDone2 ? handleFight2Click : null}
+                          className={`Map-places P1A2 ${fightDone2 ? "disabled" : ""}`}
+                        >
+                          Fight
+                        </div>
+                        )}
+                        {!TownDone && (
+                          <div
+                          onClick={!EncounterDone && fightDone ? handleEncounterClick : null}
+                          className={`Map-places P1A3 ${EncounterDone || !fightDone ? "disabled" : ""}`}
+                        >
+                          Encounter
+                        </div>
+                        )}
+                        <div 
+                          onClick={!TownDone && EncounterDone && fightDone ? handleTownClick : null}
+                          className={`Map-places P1A4 ${TownDone || !EncounterDone || !fightDone ? "disabled" : ""}`}
+                        >
+                          Town1
+                        </div>
+                      </div>
                     </div>
                   )}
-                  <div className="GameBoard">
-                    <div className="Swamp-water"><p className="swamp-p">SWAMP</p></div>
-                    <div className="Swamp-water2"><p className="swamp-p2">SWAMP</p></div>
-                    <div className="map-Path"></div>
-                    {!EncounterDone && (
-                    <div
-                      onClick={!fightDone ? handleFightClick : null}
-                      className={`Map-places P1A2 ${fightDone ? "disabled" : ""}`}
-                    >
-                      Fight
-                    </div>
-                    )}
-                    {!TownDone && (
-                      <div
-                      onClick={!EncounterDone && fightDone ? handleEncounterClick : null}
-                      className={`Map-places P1A3 ${EncounterDone || !fightDone ? "disabled" : ""}`}
-                    >
-                      Encounter
-                    </div>
-                    )}
-                    <div 
-                      onClick={!TownDone && EncounterDone && fightDone ? handleTownClick : null}
-                      className={`Map-places P1A4 ${TownDone || !EncounterDone || !fightDone ? "disabled" : ""}`}
-                    >
-                      Town1
-                    </div>
-                  </div>
                 </div>
               )}
             </div>
@@ -137,6 +155,20 @@ const GamePartTwoComponent = ({ playerName, characterStats, updateCharacterStats
       )}
 
       {showFight && (
+        <div>
+          <h1>FIGHT</h1>
+          <A1Fight
+            onReturn={handleReturnToGame}
+            Enemystats={Act1EnemyStats}
+            updateEnemyStats={updateEnemyStats}
+            playerName={playerName}
+            enemyImage={goblin}
+            updateCharacterStats={updateCharacterStats}
+            characterStats={characterStats}
+          />
+        </div>
+      )}
+      {showFight2 && (
         <div>
           <h1>FIGHT</h1>
           <A1Fight
